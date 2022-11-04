f14bb92e8df716e831dc382e3dc2981c5f8740a1
CYXNNN <info@christian-egli.ch>
2022-11-04 19:38:51 +0100

    fix circular configuration

--- src/main/java/ch/egli/lovelinebackend/LovelineBackendApplication.java
+++ src/main/java/ch/egli/lovelinebackend/LovelineBackendApplication.java
@@ -2,10 +2,8 @@ package ch.egli.lovelinebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({ "ch.egli.lovelinebackend.*" })
 public class LovelineBackendApplication {
 
 	public static void main(String[] args) {
